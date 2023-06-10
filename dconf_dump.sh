5be78ae13d95a60c82df0a73042cb467c791173a
John Wreck <john.wreck@gmail.com>
2023-06-10 09:08:32 +0200

    stuff done

--- dconf_dump.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-#
-
-dconf dump /org/gnome/terminal/legacy/profiles:/ > gnome-terminal-profiles.ini
-dconf dump / | sed -n '/\[org.gnome.settings-daemon.plugins.media-keys/,/^$/p' > custom-shortcuts.ini

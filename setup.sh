ef6f6165e97d592c50c165c53e871347bd0bd897
John Wreck <john.wreck@gmail.com>
2023-07-25 20:38:55 +0200

    stuff done

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@
 for i in $(ls -1A ~/.gdots/.config/); do ln -vs ~/.gdots/.config/$i ~/.config/$i; done
 for i in $(ls -1A ~/.gdots/user/); do ln -vs ~/.gdots/user/$i ~/$i; done
 
+#papirus-folders -C black --theme Papirus-Dark
+
